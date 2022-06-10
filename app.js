1e1d3a14963e6eed637b960c9cedc55a67b4195b
Michael Schaaf <Maschaaf@hotmail.de>
2022-06-10 13:54:56 +0200

    added cart

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 function toggleMenu(icon) {
   icon.classList.toggle("open");
 }
+
+let amountCounter = 0;
+function addItems() {
+  amountCounter += 1;
+  document.querySelector(".amount").innerHTML = amountCounter;
+  console.log(amountCounter);
+}
+
+function subItems() {
+  if (amountCounter > 0) {
+    amountCounter -= 1;
+  }
+  document.querySelector(".amount").innerHTML = amountCounter;
+}
+function showCart() {
+  console.log("hello");
+  let showCart = document.querySelector(".cart");
+
+  if (showCart.style.display === "none") {
+    showCart.style.display = "block";
+  } else {
+    showCart.style.display = "none";
+  }
+}
