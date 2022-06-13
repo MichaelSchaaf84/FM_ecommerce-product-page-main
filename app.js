a505fbb6d5cb68ccc5ba7832cc7cc9323eafd9a1
Michael Schaaf <Maschaaf@hotmail.de>
2022-06-14 00:10:01 +0200

    worked on bigger solution screens

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-function toggleMenu(icon) {
-  icon.classList.toggle("open");
-}
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  burger.addEventListener("click", () => {
+    //toogle nav
+    nav.classList.toggle("nav-active");
+    //animate links
+    navLinks.forEach((link, idx) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          idx / 7 + 0.3
+        }s`;
+      }
+    });
+    // burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
 
 let amountCounter = 0;
 function addItems(sum) {
